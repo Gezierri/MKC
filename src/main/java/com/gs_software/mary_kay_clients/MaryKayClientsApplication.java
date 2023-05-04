96c5d8cfa982ddf2cd5afb1c1af0bae4174ce8e8
gezierri <gezierrismaniotto@gmail.com>
2023-05-04 12:09:09 -0300

    build the project

---
+++ src/main/java/com/gs_software/mary_kay_clients/MaryKayClientsApplication.java
@@ -0,0 +1,13 @@
+package com.gs_software.mary_kay_clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaryKayClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaryKayClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gs_software/mary_kay_clients/MaryKayClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs_software.mary_kay_clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaryKayClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
